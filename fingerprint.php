b702aedc4087c04b9b4f338217db914f4f0d8918
Iksi <jur@iksi.cc>
2016-05-17 22:03:43 +0200

    Update for Kirby 2.3.0
    Update for Kirby 2.3.0

--- fingerprint.php
+++ fingerprint.php
@@ -3,72 +3,114 @@
 /**
  * Fingerprint css and js assets
  *
- * @version 1.3
+ * @version 1.4
  * @author Iksi <info@iksi.cc>
  */
 
-if (c::get('fingerprint', false) === false) {
+namespace Iksi;
+
+use c;
+use f;
+use HTML;
+
+if ( ! c::get('fingerprint')) {
     return;
 }
 
-$kirby      = kirby::instance();
-$cssHandler = $kirby->option('css.handler');
-$jsHandler  = $kirby->option('js.handler');
+class FingerprintCSS extends \Kirby\Component\CSS {
 
-$kirby->options['css.handler'] = function($url, $media = null) use($cssHandler, $kirby) {
+  /**
+   * Builds the html link tag for the given css file
+   * 
+   * @param string $url
+   * @param null|string $media
+   * @return string
+   */
+  public function tag($url, $media = null) {
 
-    if (is_array($url)) {
-        $css = array();
-        foreach ($url as $u) $css[] = call($kirby->option('css.handler'), $u);
-        return implode(PHP_EOL, $css) . PHP_EOL;
+    if(is_array($url)) {
+      $css = array();
+      foreach($url as $u) $css[] = $this->tag($u, $media);
+      return implode(PHP_EOL, $css) . PHP_EOL;
     }
 
-    if ($url === '@auto') {
-        $file = $kirby->site()->page()->template() . '.css';
-        $root = $kirby->roots()->autocss() . DS . $file;
-        $url  = $kirby->urls()->autocss() . '/' . $file;
+    // auto template css files
+    if($url == '@auto') {
 
-        if ( ! file_exists($root)) return false;
+      $file = $this->kirby->site()->page()->template() . '.css';
+      $root = $this->kirby->roots()->autocss() . DS . $file;
+      $url  = $this->kirby->urls()->autocss() . '/' . $file;
 
-        $url = preg_replace('#^' . $kirby->urls()->index() . '/#', null, $url);
+      if(!file_exists($root)) return false;
+
+      $url = preg_replace('#^' . $this->kirby->urls()->index() . '/#', null, $url);
+
     }
 
     if (file_exists($url)) {
-        $modifier = md5_file($url);
-        $filename = f::name($url) . '.' . $modifier . '.' . f::extension($url);
-        $dirname  = f::dirname($url);
+      $modifier = md5_file($url);
+      $filename = f::name($url) . '.' . $modifier . '.' . f::extension($url);
+      $dirname  = f::dirname($url);
 
-        $url = ($dirname === '.') ? $filename : $dirname . '/' . $filename;
+      $url = ($dirname === '.') ? $filename : $dirname . '/' . $filename;
     }
 
-    return call($cssHandler, array($url, $media));
-};
+    return html::tag('link', null, array(
+      'rel'   => 'stylesheet',
+      'href'  => url($url),
+      'media' => $media
+    ));
 
-$kirby->options['js.handler'] = function($src, $async = false) use($jsHandler, $kirby) {
+  }
 
-    if (is_array($src)) {
-        $js = array();
-        foreach($src as $s) $js[] = call($kirby->options['js.handler'], $s);
-        return implode(PHP_EOL, $js) . PHP_EOL;
+}
+
+class FingerprintJS extends \Kirby\Component\JS {
+
+  /**
+   * Builds the html script tag for the given javascript file
+   * 
+   * @param string $src
+   * @param boolean async
+   * @return string
+   */
+  public function tag($src, $async = false) {
+
+    if(is_array($src)) {
+      $js = array();
+      foreach($src as $s) $js[] = $this->tag($s, $async);
+      return implode(PHP_EOL, $js) . PHP_EOL;
     }
 
-    if ($src === '@auto') {
-        $file = $kirby->site()->page()->template() . '.js';
-        $root = $kirby->roots()->autojs() . DS . $file;
-        $src  = $kirby->urls()->autojs() . '/' . $file;
+    // auto template css files
+    if($src == '@auto') {
 
-        if ( ! file_exists($root)) return false;
+      $file = $this->kirby->site()->page()->template() . '.js';
+      $root = $this->kirby->roots()->autojs() . DS . $file;
+      $src  = $this->kirby->urls()->autojs() . '/' . $file;
 
-        $src = preg_replace('#^' . $kirby->urls()->index() . '/#', null, $src);
+      if(!file_exists($root)) return false;
+
+      $src = preg_replace('#^' . $this->kirby->urls()->index() . '/#', null, $src);
+
     }
 
     if (file_exists($src)) {
-        $modifier = md5_file($src);
-        $filename = f::name($src) . '.' . $modifier . '.' . f::extension($src);
-        $dirname  = f::dirname($src);
+      $modifier = md5_file($src);
+      $filename = f::name($src) . '.' . $modifier . '.' . f::extension($src);
+      $dirname  = f::dirname($src);
 
-        $src = ($dirname === '.') ? $filename : $dirname . '/' . $filename;
+      $src = ($dirname === '.') ? $filename : $dirname . '/' . $filename;
     }
 
-    return call($jsHandler, array($src, $async));
-};
+    return html::tag('script', '', array(
+      'src'   => url($src),
+      'async' => $async
+    ));
+
+  }
+
+}
+
+$kirby->set('component', 'css', 'Iksi\FingerprintCSS');
+$kirby->set('component', 'js', 'Iksi\FingerprintJS');
